f94f1fb2e1cd8a17c2dbb121087a43bfdc69160b
xinyihe4 <xinyihe4@fa22-cs527-018.cs.illinois.edu>
2022-10-16 22:51:45 -0500

    add find test script

--- nifi/findTests.py
+++ nifi/findTests.py
@@ -2,38 +2,30 @@ import constants
 import argparse
 import os
 
-def find_test(input_path, output_path):
+def find_test(input_path):
 
     test_names = []
     find = False
     with open(input_path, 'r') as test_file:
         for line in test_file:
             if 'public class' in line:
-                test_class = line.strip('/n').split(" "):
-                for word in test_class:
-                    if word[-1::] == ("{"):
-                        test_names.append(word.replace("{", ""))
-                        break
+                test_class = line.split(" ")
+                test_names.append(test_class[2] + "\n")
+                test_names.append(input_path + "\n")
             if '@Test' in line:
-                test_name_line = next(test_file)
-                test_name_line.strip('/n')
-                test_name_line.split(" ")
+                test_name_line = next(test_file).split(" ")
                 for word in test_name_line:
-                    if word[-2::] == ("{"):
-                        test_names.append(word.replace("()", ""))
+                    if word[-2:] == ("()"):
+                        test_names.append(word.replace("()", "\n"))
                         break
-    with open(output_path, 'a') as output_file:
+    with open("test/" + test_names[0]+".txt", 'w') as output_file:
         output_file.writelines(test_names)
 
-    print("Finished!")
+    print("Finished!" + "Total test number is " + str(len(test_names) - 2))
     return
-
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Input Test file and output file name')
     parser.add_argument('--TEST_FILE', '--t', type=str, required = True, help='The path of Test file')
-    parser.add_argument('--OUTPUT_PATH', '--o', type=str, required = True, help='The path of Output file')
     args = parser.parse_args()
-    find_test(args.TEST_FILE, args.OUTPUT_PATH)
+    find_test(args.TEST_FILE)
