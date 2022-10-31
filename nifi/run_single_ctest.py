71ce8cbb78decd4cf17928d82234e83112ce890d
xinyihe4 <xinyihe4@fa22-cs527-018.cs.illinois.edu>
2022-10-30 19:44:28 -0500

    inspect and run

--- nifi/run_single_ctest.py
+++ nifi/run_single_ctest.py
@@ -2,17 +2,15 @@ import sys, time
 import os
 import constants
 
-def main(argv):
-
-    with open('../../nifi/nifi-commons/nifi-properties/src/test/resources/NiFiProperties/conf/ctest.properties', 'w') as f:
-        params = '\n'.join(argv[2:])
-        f.write(params)
-
+def run_single_test(argv):
     command = constants.CD_NIFI_PATH
     command += constants.MVN_INSTALL_NIFI_COMMONS
-    cur_test_name = argv[1]
+    cur_test_name = argv
     command += 'mvn -pl nifi-commons/nifi-properties/ test -Dtest="'+cur_test_name+'"'
     os.system(command)
 
 if __name__ == "__main__":
-    main(sys.argv)  
+    with open('../../nifi/nifi-commons/nifi-properties/src/test/resources/NiFiProperties/conf/ctest.properties', 'w') as f:
+        params = '\n'.join(sys.argv[2:])
+        f.write(params)
+    run_single_test(sys.argv[1])  
